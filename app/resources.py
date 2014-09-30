0e771313cd19f69d2414174c020e10010dff4c24
Halfdan Rump <halfdan-rump@garage.co.jp>
2014-09-30 15:47:49 +0900

    Pimps ApiReponse && fixes node POST

--- app/resources.py
+++ app/resources.py
@@ -1,6 +1,6 @@
 from app import rest_api, flapp
 from satoyama.models import Node, Sensor, Reading
-
+import exc
 from flask.ext import restful
 from flask import request
 from sqlalchemy.exc import DataError
@@ -13,112 +13,124 @@ API_UNITS = {
 	's':'SI seconds'
 	}
 
-def get_form_data(response, field = None):
-	assert isinstance(response, ApiResponse), 'response must an instance of type ApiResponse'
-	try:
-		field = request.form[field]
-	except KeyError:
-		response.add_warning('Missing field: %s'%field)
-		field = None
-	finally:
-		return field
 
 class ApiResponse(object):
 	
+	__fields__ = ['warnings', 'errors', 'objects', 'query']
+
 	def __init__(self, request):
 		self.warnings = list()
 		self.errors = list()
-		self.request_data = request.form
 		self.objects = list()
+		self.query = request.form
+		
 
-	def add_warning(self, warning):
-		self.warnings.append(warning)
+	def __append__(self, listname, item_to_append):
+		if hasattr(item_to_append, 'json'):
+			item = item_to_append.json()
+		elif hasattr(item_to_append, 'message'):
+			
+			item = item_to_append.message
+		else:
+			try:
+				item = str(item_to_append)
+			except Exception:
+				item = exc.InternalErrorException().json()
+		getattr(self, listname).append(item)
 
-	def add_error(self, error):
-		self.errors.append(error)
+
+	def add_warning(self, warning_message):
+		self.__append__('warnings', warning_message)
+
+	def add_error(self, exception):
+		self.__append__('errors', exception)
 
 	def add_object(self, obj):
-		if hasattr(obj, 'json_detailed'):
-			self.objects.append(obj.json_detailed())
-		else:
-			self.objects.append(obj)
+		self.__append__('objects', obj)
 		
-
 	def json(self):
-		return {'warnings': self.warnings, 'errors': self.errors, 'request': self.request_data, 'data': self.objects}
+		return dict(zip(ApiResponse.__fields__, map(lambda x: getattr(self, x), ApiResponse.__fields__)))
+
+		# {'warnings': self.warnings, 'errors': self.errors, 'request': self.request_data, 'data': self.objects}
 
 
 
-class ApiBaseException(Exception):
-	def __init__(self, message = None):
-		super(ApiBaseException, self).__init__(message)
-		
-		def __repr__(self):
-			return self.message
+def get_form_data(response, field_name, field_type):
+	"""
+	Helper function for getting and type-validating a named query parameter from HTTP request.
 
-		### Do some crazy logging in here
-
-
-class UnknownUnitException(ApiBaseException):
-	def __init__(self, supplied_unit):
-		super(UnknownUnitException, self).__init__('Bad unit specified: %s'%supplied_unit)
-
-
-class MissingNodeException(ApiBaseException):
-	def __init__(self, node_id):
-		super(MissingNodeException, self).__init__('No such node: %s'%node_id)
-
-
-class Unit:
-	def __init__(self, unit_string):
-		self.unit = unit_string
-
-	def verify_unit(self):
-		if not self.unit in API_UNITS.keys() or self.unit not in API_UNITS.values():
-			raise UnknownUnitException(self.unit)
+	:param response: ApiResponse object
+	:paran field_name: The name of the query parameter
+	:param field_type: data type of the field. Must be a Python builtin type, e.g., int, str, etc.
+	"""
+	assert isinstance(response, ApiResponse), 'response must an instance of type ApiResponse'
 	
-	def get_unit_name(self):
-		return API_UNITS[self.unit]
+	try:
+		field = request.form[field_name]
+	except KeyError:
+		response.add_error('Missing field: %s'%field_name)
+		field = None
+	
+	try: 
+		field = field_type(field)
+	except ValueError, e:
+		flapp.logger.exception(e)
+	finally:
+		return field
 
-	def get_unit_description(self):
-		pass
 
-	def __repr__(self):
-		return str(self.unit)
+
+
+
+
+
+
+
+# class Unit:
+# 	def __init__(self, unit_string):
+# 		self.unit = unit_string
+
+# 	def verify_unit(self):
+# 		if not self.unit in API_UNITS.keys() or self.unit not in API_UNITS.values():
+# 			raise UnknownUnitException(self.unit)
+	
+# 	def get_unit_name(self):
+# 		return API_UNITS[self.unit]
+
+# 	def get_unit_description(self):
+# 		pass
+
+# 	def __repr__(self):
+# 		return str(self.unit)
+
+	
 
 
 
 class NodeResource(restful.Resource):
 	def get(self):
 		response = ApiResponse(request)
-		node_id = get_form_data(response, 'id')
-		try:
-			node_id = int(node_id)
-		except ValueError:
-			response.add_error('node_id must be an integer')
+		node_id = get_form_data(response, 'node_id', int)
+		print 
 		node = Node.query.filter_by(id = node_id).first()
 		if node:
 			response.add_object(node)
 		else:
-			response.add_error('no such node')
+			response.add_error(exc.MissingNodeException(node_id))
 		return response.json()
 			
 		
-
 	def post(self):
 		response = ApiResponse(request)
-		try:
-			node = Node.create()
-			response.add_object(node)
-		except Exception, e:
-			response.add_error(e)
+		params = flapp.check_query_parameters(Node, response)
+		node = Node.create(**params)
+		response.add_object(node)
 		return response.json()
 		
 rest_api.add_resource(NodeResource, '/node')		
 
 class SensorResource(restful.Resource):
 	
-
 	def get(self, alias, sensor_type):
 		""" 
 		REST GET handler. Query database and return json dump of retrieved object(s)
@@ -170,7 +182,6 @@ class ReadingResource(restful.Resource):
 		else:
 			# try:
 			reading = Reading.query.filter_by(sensor = sensor).all()[-1]
-			# print reading.json_detailed()
 			response.add_object({'value': reading.value})
 			# except Exception, e:
 			# 	response.add_error(e.message)
