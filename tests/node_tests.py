99cc1f9c3e2cc3627955695fad6fadf11d6bec53
Halfdan Rump <halfdan-rump@garage.co.jp>
2014-10-07 10:48:27 +0900

    More refactoring. Made helper module for tests and place test base class in there

--- tests/node_tests.py
+++ tests/node_tests.py
@@ -4,36 +4,8 @@ import requests
 from satoyama.models import Node, Sensor, SensorType, Reading
 from app import flapp
 import json
+from test_helpers import ApiTester
 
-
-
-class ApiTester(object):
-	def assert_response_format(self, response):
-		"""
-		:param response: An instance of requests.Response
-		Checks if an ApiResponse can be created
-		from the text attribute of the response
-		"""
-		json_dict = json.loads(response.text)
-		self.assertTrue(json_dict.has_key('warnings'))
-		self.assertTrue(json_dict.has_key('errors'))
-		self.assertTrue(json_dict.has_key('objects'))
-
-	def assert_all_ok(self, response):
-		"""
-		Takes a requests.Response instance and checks if the status code is OK. ALSO checks if an ApiResponse can be created
-		from the text attribute of the response, and if so, whether or not the ApiResponse status is OK. 
-		"""
-		self.assertTrue(response.ok)
-		self.assert_response_format(response)
-		api_response = self.get_api_response(response)
-		self.assertTrue(api_response.ok)
-		return api_response
-
-	def get_api_response(self, response):
-		json_dict = json.loads(response.text)
-		api_response = app.resources.ApiResponse(**json_dict)
-		return api_response
 
 
 class NodeResourceTests(unittest.TestCase, ApiTester):
@@ -48,6 +20,7 @@ class NodeResourceTests(unittest.TestCase, ApiTester):
 		# r = requests.get(url)
 		r = requests.get(url, data = {'node_id' : 1})
 		self.assert_all_ok(r)
+		print 'ASDLIAJSDLAKJSDOLAKSJDLAKSJDASJD'
 
 	# def test_GET_nonexisting_node_by_id(self):
 	# 	url = flapp.get_url('node')
