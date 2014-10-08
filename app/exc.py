4bc498e39b8c08af13c69f093257a0ebb881c701
Halfdan Rump <halfdan-rump@garage.co.jp>
2014-10-08 14:57:09 +0900

    Removes superfluous exception and renames base exception

--- app/exc.py
+++ app/exc.py
@@ -1,31 +1,28 @@
 import json
 
-class ApiBaseException(Exception):
-	def __init__(self, message = 'Something went wrong. Please call customer support.'):
-		super(ApiBaseException, self).__init__(message)
+class ApiException(Exception):
+	def __init__(self, message = 'Sorry, something went wrong. Please call technical support!'):
+		super(ApiException, self).__init__(message)
 		
 	def json(self):
 		return self.message
-		#
+		### Do some crazy logging in here, based on the exception class?
 
-		### Do some crazy logging in here
 
-class InternalErrorException(ApiBaseException):
-	def __init__(self):
-		super(InternalErrorException, self).__init__('Sorry, something went wrong. Please call technical support!')
+class UnknownUnitException(ApiException):
+	def __init__(self, supplied_unit = None):
+			super(UnknownUnitException, self).__init__('Bad unit specified: %s'%supplied_unit)
 
-class UnknownUnitException(ApiBaseException):
-	def __init__(self, supplied_unit):
-		super(UnknownUnitException, self).__init__('Bad unit specified: %s'%supplied_unit)
 
-class InvalidAttributeException(ApiBaseException):
+class InvalidAttributeException(ApiException):
 	def __init__(self, attribute, model = None):
 		message = '"%s" has no meaning or is not allowed the context of the current request.'%attribute
 		if model: 
 			message += ' Valid parameters for model %s are: %s'%(model.__name__, model.settables())
 		super(InvalidAttributeException, self).__init__(message)
 
-class MissingNodeException(ApiBaseException):
+
+class MissingNodeException(ApiException):
 	def __init__(self, node_id):
 		super(MissingNodeException, self).__init__('No such node: %s'%node_id)
 
