ee7b18137b773263fe82b3d7e6e923524712fba8
Halfdan Rump <halfdan-rump@garage.co.jp>
2014-10-10 12:20:41 +0900

    Adds WORKING migrations :))

--- manage.py
+++ manage.py
@@ -2,13 +2,25 @@ from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
 import os
 
-from app import flapp, db
-app.config.from_object(os.environ['APP_SETTINGS'])
+from app import flapp
+import app
+# flapp.config.from_object(os.environ['APP_SETTINGS'])
+app.conf.config_development(flapp)
 
-migrate = Migrate(flapp, db)
+from satoyama.models import *
+from satoyama.database import Base
+
+# models = satoyama.database.get_defined_models()
+
+# for model in models:
+# 	migrate = Migrate(flapp, model)
+# 	manager = Manager(flapp)
+
+migrate = Migrate(flapp, Base)
 manager = Manager(flapp)
 
 manager.add_command('db', MigrateCommand)
+
 
 if __name__ == '__main__':
     manager.run()
