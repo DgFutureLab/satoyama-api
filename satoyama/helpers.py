af7d158ac27f1d83e8a2d68e5a0b77fab995dace
Halfdan Rump <halfdan-rump@garage.co.jp>
2014-10-15 11:38:57 +0900

    Adds helper to convert from datetime to timestamp

--- satoyama/helpers.py
+++ satoyama/helpers.py
@@ -5,13 +5,17 @@ from definitions import DATETIME_FORMATS
 
 class DatetimeHelper(HelperBase):
 
-	def convert_timestamp(self, timestamp):
+	@staticmethod
+	def convert_timestamp_to_datetime(timestamp):
 		"""
+		Convert a string timestamp to a datetime instance. If timestamp is already a datetime instance, the
+		method just returns the same instance.
 		:param timestamp: Must be an instance of datetime, or a string with one of the allowed satoyama datetime formats.
 		"""
 		converted = False
 		if not isinstance(timestamp, datetime):
 			for format in DATETIME_FORMATS:
+				print format
 				try:
 					timestamp = datetime.strptime(timestamp, format)
 					converted = True
@@ -26,3 +30,10 @@ class DatetimeHelper(HelperBase):
 		else:
 			# self.messages.append('Provided timestamp matched none of the allowed datetime formats. Using local server time.')
 			return datetime.utcnow()
+
+	@staticmethod
+	def convert_datetime_to_timestamp(datetime_instance):
+		"""
+		Converts a datetime instance into a timestamp string with the highest precision format
+		"""
+		return datetime_instance.strftime(DATETIME_FORMATS[0])
