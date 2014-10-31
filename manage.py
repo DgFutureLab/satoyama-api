f2b4468e48fb011092220180e54d1d9949566cea
Halfdan Rump <halfdan-rump@garage.co.jp>
2014-10-31 16:33:14 +0900

    Creates initial migration

--- manage.py
+++ manage.py
@@ -8,18 +8,35 @@ from satoyama.models import *
 from satoyama.database import Base
 
 
+
+# parser = argparse.ArgumentParser()
+# parser.add_argument('--env', choices = ('test', 'dev', 'prod'), required = True, help = 'Specify environment, which determines which database to use.')
+# args = parser.parse_args()
+# environment = args.env
+# db_manager.set_environment(environment)
+
 migrate = Migrate(flapp, Base)
 migration_manager = Manager(flapp)
 
+# class Print(Command):
+
+#         def run(self):
+#             print "hello"
+
+# def test():
+# 	print 'HUUUUUUUUUUUUU'
+
+
+
+# migration_manager.add_command('test', Print)
 migration_manager.add_command('db', MigrateCommand)
+# 
+
+
 
 
 if __name__ == '__main__':
-	# parser = argparse.ArgumentParser()
-	# parser.add_argument('--env', choices = ('test', 'dev', 'prod'), required = True, help = 'Specify environment, which determines which database to use.')
-	# args = parser.parse_args()
-	# environment = args.env
-	# db_manager.set_environment(environment)
+
 	
 
 	migration_manager.run()
--- migrations/env.py
+++ migrations/env.py
@@ -16,6 +16,9 @@ fileConfig(config.config_file_name)
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 from flask import current_app
+
+import argparse
+
 config.set_main_option('sqlalchemy.url', current_app.config.get('SQLALCHEMY_DATABASE_URI'))
 target_metadata = current_app.extensions['migrate'].db.metadata
 
