d7b7fad8ce02cf6de6a82cdc223fd8d45c93eaed
Halfdan Rump <halfdan-rump@garage.co.jp>
2014-12-01 13:01:17 +0900

    Cleans up run_webserver

--- run_webserver.py
+++ run_webserver.py
@@ -1,24 +1,11 @@
 import argparse
 from app import flapp, conf
-from seeds.sites import SiteSeeder
-from multiprocessing import Process
 
 def run_webserver(name, env):
 	conf.configure_flapp(env)
 	flapp.logger.debug('Running webserver with config: %s'%flapp.config)
 	flapp.run(port = flapp.config['PORT'])
 
-
-def run_simulation(site_id):
-	print '*******************************   Simulating site %s'%site_id
-	p = Process(target = SiteSeeder.simulate_ricefield_site, args = (site_id,))
-	p.start()
-
-import time, pytest
-def run_tests():
-	time.sleep(2)
-	print 'RUNNING TTEEEEEEEEEEEEEEESTS!!!'
-	pytest.main()
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--env', choices = ('test', 'dev', 'prod'), required = True, help = 'Specify environment, which determines which database to use.')
@@ -27,12 +14,5 @@ args = parser.parse_args()
 environment = args.env
 site_id = args.simulation_site
 
-print 'SSIIIIIIIIIIIIIITE'
-print site_id
-
 if __name__ == "__main__":
-	# run_simulation(site_id)
-
-	# Process(target = run_webserver, args = ('webserver', environment, )).start()
-	# Process(target = run_tests).start()
 	run_webserver('webserver', environment)
