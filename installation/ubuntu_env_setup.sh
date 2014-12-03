ac41cdff0154f278e1c9c04379cbb03a725c24db
Halfdan Rump <halfdan-rump@garage.co.jp>
2014-12-03 12:56:48 +0900

    Bugfix

--- installation/ubuntu_env_setup.sh
+++ installation/ubuntu_env_setup.sh
@@ -2,7 +2,6 @@
 virtualenv ../env
 source ../env/bin/activate
 
-git clone https://github.com/DgFutureLab/satoyama-api.git
-pip install -r satoyama-api/requirements.txt
+pip install -r requirements.txt
 cp db_config_sample.yml db_config.yml
 python satoyama-api/manage.py db upgrade
