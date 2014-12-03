af50b8a7af759530850bca1bcbcde31e1685ffa4
Halfdan Rump <halfdan-rump@garage.co.jp>
2014-12-03 15:33:04 +0900

    Updates install script

--- installation/ubuntu_env_setup.sh
+++ installation/ubuntu_env_setup.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-virtualenv ../env
-source ../env/bin/activate
-
+startpath="/root/satoyama-api"
+echo $startpath
+envpath="/root/"
+envname="satoyama-env"
+cd $envpath
+virtualenv $envname
+echo $envpath$envname"/bin/activate"
+. $envpath$envname"/bin/activate"
+cd $startpath
 pip install -r requirements.txt
-cp db_config_sample.yml db_config.yml
-python satoyama-api/manage.py db upgrade
+#cp db_config_sample.yml db_config.yml
+#python satoyama-api/manage.py db upgrade
