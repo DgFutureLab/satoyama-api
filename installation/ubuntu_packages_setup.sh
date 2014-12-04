8a58400d1dc34981cf40eed55c9fed6853b1c76d
Halfdan Rump <halfdan-rump@garage.co.jp>
2014-12-04 10:58:34 +0900

    Updates install script for ubuntu and adds postgres install step

--- installation/ubuntu_packages_setup.sh
+++ installation/ubuntu_packages_setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install -y libpq-dev postgresql postgresql-contrib
+sudo apt-get install python-psycopg2
 sudo apt-get install -y git
 sudo apt-get install -y python-setuptools python-dev
 sudo easy_install virtualenv
--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#sh installation/ubuntu_packages_setup.sh
-#sh installation/ubuntu_db_setup.sh
+sh installation/ubuntu_packages_setup.sh
+sh installation/ubuntu_db_setup.sh
 sh installation/ubuntu_env_setup.sh
