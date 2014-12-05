54cf18de845c4c48bbf3c12e1d7b84c4daedde4e
Halfdan Rump <halfdan-rump@garage.co.jp>
2014-12-05 18:08:58 +0900

    Simplifies dependencies...

--- installation/ubuntu_packages_setup.sh
+++ installation/ubuntu_packages_setup.sh
@@ -8,10 +8,7 @@ ERROR="$6"
 
 echo "${ATTENTION} Installing Ubuntu packages. This may take a while. ${DEFAULT}"
 sudo apt-get update -qq
-sudo apt-get install -y -qq libpq-dev postgresql postgresql-contrib
-sudo apt-get install -y -qq python-psycopg2
-sudo apt-get install -y -qq git
-sudo apt-get install -y -qq python-setuptools python-dev
+sudo apt-get install -y -qq libpq-dev postgresql postgresql-contrib python-setuptools python-dev
 sudo apt-get build-dep -y -qq python-psycopg2
 sudo easy_install virtualenv
 echo "${SUCCESS} Installed Ubuntu packages. ${DEFAULT}"
