f479bfde8d2ea0104005a22c473d01536c1d63d2
Halfdan Rump <halfdan-rump@garage.co.jp>
2014-12-05 18:40:45 +0900

    Adds dev and prod dbs

--- installation/ubuntu_db_setup.sh
+++ installation/ubuntu_db_setup.sh
@@ -12,6 +12,8 @@ sudo -u postgres psql -c "create role root with superuser login"
 sudo -u postgres psql -c "create database root"
 sudo -u postgres psql -c "create role satoyama with login superuser"
 sudo -u postgres psql -c "alter role satoyama with password 'satoyama'"
+sudo -u postgres psql -c "create database satoyama_dev"
 sudo -u postgres psql -c "create database satoyama_test"
+sudo -u postgres psql -c "create database satoyama_prod"
 
 echo "${SUCCESS} Database setup complete!"
