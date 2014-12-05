833c3b7a1be50238253eb07570ebddf440e3e938
Halfdan Rump <halfdan-rump@garage.co.jp>
2014-12-05 11:25:03 +0900

    Further awesomeifies install script

--- installation/ubuntu_env_setup.sh
+++ installation/ubuntu_env_setup.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
-startpath="/root/satoyama-api"
-echo $startpath
-envpath="/root/"
-envname="satoyama-env"
-cd $envpath
-virtualenv $envname
-echo $envpath$envname"/bin/activate"
-. $envpath$envname"/bin/activate"
-cd $startpath
-pip install -r requirements.txt
-#cp db_config_sample.yml db_config.yml
+apifolder="$1"
+envname="$2"
+echo "Creating new Python environment named $envname"
+
+virtualenv "~/$envname"
+if [ -e "~/$envname/bin/activate" ]; then
+	. "~/$envname/bin/activate"
+	echo "Environment changed successfully! Now using Python interpreter: `which python`"
+else
+	echo "\n\n\n Something went wrong. Could not find activate script for new environment. Exitting..."
+	exit 1
+fi
+
+
+
+if [ -e "$apifolder/requirements.txt" ]; then
+	echo "Installing Python modules in new environment..."
+	pip install -r "$apifolder/requirements.txt"
+else
+	echo "\nCould not find requirements.txt. Exitting..."
+	exit 1
+fi
+
+if [ -e "$apifolder/db_config_sample.yml" ]; then
+	echo "\n Copying db_config_sample.yml to db_config.yml"
+	echo "****** ATTENTION! You can db_config.yml with your own settings, but it is not necessarry."
+	cp "$apifolder/db_config_sample.yml" "$apifolder/db_config.yml"
+else
+	echo "Could not find db_config_sample.yml. Exitting..."
+	exit 1
+fi
+
+
 #python satoyama-api/manage.py db upgrade
--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
-if [ -z "$1" ]; then
+
+envname="satoyama-env"
+
+apifolder="$1"
+# startloc=`pwd`
+if [ -z "$apifolder" ]; then
 	echo Please specify the path to the satoyama-api folder, for instance: sh satoyama-api/setup_ubuntu.sh satoyama-api/
 fi 
 
-if [ -d "$1" ]; then
-	echo Using "$1"
-	cd "$1"
-	if [ -e wercker.yml ]; then
+if [ -d "$apifolder" ]; then
+	echo Using "$apifolder"
+	if [ -e "$apifolder/wercker.yml" ]; then
 		echo Folder seems to be correct.
 	else
 		echo Folder has no wercer.yml file. Are you sure you are specifying the right path? Exitting...
 		exit 1
-		echo hun
 	fi
 else
-        echo $1: Folder does not exist! Exitting..
+        echo $apifolder: Folder does not exist! Exitting..
 	exit 1
 fi
+
 #sh installation/ubuntu_packages_setup.sh
 #sh installation/ubuntu_db_setup.sh
-#sh installation/ubuntu_env_setup.sh
+sh "$apifolder/installation/ubuntu_env_setup.sh" "$apifolder" "$envname"
