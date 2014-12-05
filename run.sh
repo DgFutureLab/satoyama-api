70f8ee066b42da74c4f7c8cb6985b307dfc9acda
Halfdan Rump <halfdan-rump@garage.co.jp>
2014-12-05 09:33:45 +0900

    Removes unused shell script

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source /Users/halfdan-rump/projects/env/bin/activate
-#source /home/halfdan/env/bin/activate
-which python
-python simulate_kamogawa_ricefield.py
-# python run_webserver.py --env test
