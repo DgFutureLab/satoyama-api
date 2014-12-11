189b620e921c210028c5faf57b8f9fac0c2b19da
Halfdan Rump <halfdan-rump@garage.co.jp>
2014-12-11 15:54:21 +0900

    Adds POST and GET tests for Site resource

--- app/tests/test_site_resource.py
+++ app/tests/test_site_resource.py
@@ -4,7 +4,7 @@ from app import flapp
 from satoyama.tests.dbtestbase import DBTestBase
 from seeds.nodes import NodeSeeder
 from seeds.sites import SiteSeeder
-from app.helpers import ApiResponseHelper
+from app.apihelpers import UrlHelper, ApiResponseHelper
 
 class SiteResourceTests(DBTestBase):
 
@@ -14,16 +14,23 @@ class SiteResourceTests(DBTestBase):
 
 
 	def test_GET_existing_site_by_id(self):
-	 	Site.create() # create the site that we want to GET
-	 	url = flapp.get_url('site/1')
-	 	r = requests.get(url)
-	 	api_response = ApiResponseHelper.assert_all_ok(r)
-	 	site_json = api_response.objects[0]
-	 	for attr in Site.columns():
-	 		assert site_json.has_key(attr)
+	 	site = SiteSeeder.seed_empty_site()
+	 	url = UrlHelper.get_url(flapp, 'site', site.id)
+	 	response = requests.get(url)
+	 	assert response.ok
+	 	api_response = ApiResponseHelper.assert_api_response(response)
+	 	assert api_response.first() == site.json()
 
 	def test_GET_nonexisting_site_by_id(self):
-	 	url = flapp.get_url('site/1')
+	 	url = UrlHelper.get_url(flapp, 'site', 123017)
 	 	response = requests.get(url)
-	 	api_response = ApiResponseHelper.assert_all_ok(response, expect_success = False)
+	 	assert response.ok
+	 	api_response = ApiResponseHelper.assert_api_response(response, expect_success = False)
 	 	
+	def test_POST_empty_site(self):
+	 	url = UrlHelper.get_url(flapp, 'node')
+		data = {'alias':'Kamogawa_east'}
+		response = requests.post(url, data = data)
+		assert response.ok
+		api_response = ApiResponseHelper.assert_api_response(response)
+		assert api_response.first()['alias'] == 'Kamogawa_east'
