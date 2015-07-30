258462062a3f0289a5a5434fe0379b2fb0bec84a
Halfdan Rump <halfdan-rump@garage.co.jp>
2015-07-30 16:10:28 +0900

    Fixes migration scripts. Now migration also works in production

--- migrate_prod.py
+++ migrate_prod.py
@@ -1,13 +1,14 @@
 from app import flapp, conf
 from satoyama.database import manager as db_manager
-conf.configure_flapp('development')
+conf.configure_flapp('production')
 
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
 import argparse
+
+
 from satoyama.models import *
 from satoyama.database import Base
-
 
 migrate = Migrate(flapp, Base)
 migration_manager = Manager(flapp)
